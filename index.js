71d09429c1a9fd02c0a2aeb647d4f3d174c490cd
debanjo <debanjo31@gmail.com>
2022-07-04 21:59:10 +0100

    SOCIAL MEDIA LINK UPDATED

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ $(document).ready(function(){
       
         if($(this).is(":checked")){  
           $("body").get(0).style.setProperty("--white","rgb(255, 255, 255)")
-          $("body").get(0).style.setProperty("--black","rgb(38, 40, 41)")
+          $("body").get(0).style.setProperty("--black","rgb(0, 0, 36)")
         }
         else if($(this).is(":not(:checked)")){
           $("body").get(0).style.setProperty("--white","rgb(255, 255, 255)")
